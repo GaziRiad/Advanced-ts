896fa94fd82b172e7b1404571cb6a71e51b1809b
GaziRiad <ouhibimustapha36@gmail.com>
2024-04-21 11:51:03 +0100

    Building a better wrapper comp using ComponentPropsWithoutRef

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
+import Input from "@/components/Input";
 import React from "react";
 
 export default function page() {
-  return <div>page</div>;
+  return (
+    <main>
+      <Input id="name" label="Your name:" type="text" />
+      <Input id="age" label="Your age:" type="number" />
+    </main>
+  );
 }
